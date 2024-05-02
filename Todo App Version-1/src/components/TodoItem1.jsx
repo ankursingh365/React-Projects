622034b0e5ddf559af830497e6303e49be672091
Ankur Singh <singhankur3072002@gmail.com>
2024-05-02 23:07:08 +0530

    Project-1

---
+++ Todo App Version-1/src/components/TodoItem1.jsx
@@ -0,0 +1,23 @@
+function TodoItem1() {
+
+    let todoName='BUY MILK';
+    let todoDate='02/05/2024';
+
+    return (
+        <div class="container text-center">
+            <div class="row kg-row">
+                <div class="col-6">
+                    {todoName}
+                </div>
+                <div class="col-4">
+                    {todoDate}
+                </div>
+                <div class="col-2">
+                    <button type="button" class="btn btn-danger kg-button">Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TodoItem1;
